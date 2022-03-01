07dbb4cebf4d9e0adc71ed8f17aa1407f77c7ff7
anatolipv <anatoli.vachev@gmail.com>
2022-03-02 01:46:08 +0200

    easy for now

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(theString,timesToRepeat) {
+    if (timesToRepeat <0)
+        return 'ERROR';
+    else {
+    let answer = "";
+    for (let i = 0; i < timesToRepeat; i++) {
+        answer+=theString;
+    }
+    return answer;
+    }
 };
 
 // Do not edit below this line

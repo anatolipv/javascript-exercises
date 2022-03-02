c2d852a4937d24632e446a97d4ff91191d96ea46
anatolipv <anatoli.vachev@gmail.com>
2022-03-02 04:38:33 +0200

    a lot of goodle used ;D

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayInput,...valuesToRemove) {
+    let filtered= arrayInput.filter(value=>valuesToRemove.indexOf(value)===-1);
 
-};
+    return filtered;
+
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

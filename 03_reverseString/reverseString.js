db25f36ed1238a812d42bedb3cfe84a650971422
anatolipv <anatoli.vachev@gmail.com>
2022-03-02 02:01:26 +0200

    done with loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    let reversedString = "";
+    if(stringInput==="")
+        return "";
+    else {
+    for (let i = stringInput.length-1; i >=0; i--) {
+        reversedString += stringInput[i];
+        
+    }
+    return reversedString;
+    }
 };
 
 // Do not edit below this line

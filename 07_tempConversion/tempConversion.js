f749321c810dcd63a426cd3ae09477b884d95a05
anatolipv <anatoli.vachev@gmail.com>
2022-03-02 05:30:41 +0200

    Kinda easy

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let tempInC =(tempF-32)*5/9;
+  return parseFloat(tempInC.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let tempInF = tempC*9/5+32;
+  return parseFloat(tempInF.toFixed(1));
 };
 
 // Do not edit below this line

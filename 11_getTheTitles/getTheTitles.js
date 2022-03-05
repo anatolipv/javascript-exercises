5c650c9b3fef951f410df76e6586f7eddf93bcc8
anatolipv <anatoli.vachev@gmail.com>
2022-03-05 04:12:59 +0200

    need some help here :D

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(array) {
+    
+     const titles = array.map(()=>array.title);
+    return titles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;

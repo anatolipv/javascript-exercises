786b4b422ae0e4813895a2621c1b008c19bbf36c
anatolipv <anatoli.vachev@gmail.com>
2022-03-05 03:28:11 +0200

    Ugly but working

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    let newstring="";
+    for (let i = 0; i < a.length; i++) {
+        if(a[i]!=" " && a[i]!="."&& a[i]!=","&& a[i]!="!")
+            newstring+=a[i];
+    }
+    let reversed="";
+    for (let i = newstring.length-1; i >=0; i--){
+        reversed+=newstring[i];
+       
+    }
+    if(newstring.toLowerCase()==reversed.toLowerCase())
+        return true;
+    else
+        return false;
 };
 
 // Do not edit below this line

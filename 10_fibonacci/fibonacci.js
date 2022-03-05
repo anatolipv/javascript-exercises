36541a54080d11d6165337b62c4056a70b2fc978
anatolipv <anatoli.vachev@gmail.com>
2022-03-05 03:43:53 +0200

    Returns n fibonacci number.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let a=1;
+    let b=1;
+    let c=0;
+    let counter =2;
+    if(number ==1 || number == 2)
+        return 1;
+    else if(number <0)
+        return "OOPS";
+    else{
+        while(counter<number){
+            c=a+b;
+            a=b;
+            b=c;
+            counter++;
+        }
+        return c;
+    }
+       
 };
 
 // Do not edit below this line

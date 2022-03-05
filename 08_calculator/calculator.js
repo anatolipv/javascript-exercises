99e86de41c73bf27995df78af23e6ab7f9c48e94
anatolipv <anatoli.vachev@gmail.com>
2022-03-05 03:10:35 +0200

    Created calc functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let sum=0;
+  for (let i = 0; i < a.length; i++) {
+    sum+=a[i];    
+  }  
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let sum=1;
+  for (let i = 0; i < a.length; i++) {
+    sum*=a[i];    
+  }  
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = 1;
+	if(a==0)
+    return 1;
+  else {
+    for (let i = a; i > 0; i--) {
+      fact*=i;
+    }
+    return fact;}
 };
 
 // Do not edit below this line
